e4229131052658e7766d9faccc29a1e78a0023c6
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-07 17:56:04 +0200

    background test

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ImageBackground source={'./assets/bg.png'} resizeMode={'cover'} style={styles.image}>
+        <Text style={{height:'48px', display:'flex', justifyContent:'center', alignItems:'center'}}>Open up App.tsx to start working on your app!</Text>
+        
+        <View style={styles.gradientContainer}></View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +17,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    width: '100%', height: '100%',
+    justifyContent: 'center',
+  },
+  gradientContainer:{
+    flex:1,
+    width:'100%',
+    height:'100%',
+    backgroundColor:'#000'
+  }
 });
